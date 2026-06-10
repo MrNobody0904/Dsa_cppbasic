574cd213b79d028fe0f39e936cdb1416b57bc30a
MrNobody0904 <pranavraj20@gmail.com>
2026-06-10 23:33:47 +0530

    q20

---
+++ cpp_DSA/cpp/cpp_basic/practise20.cpp
@@ -0,0 +1,14 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    // star pattern 2;
+    for(int i =0;i<6;i++){
+        for(int j=0;j<6-i;j++){ // ulta kar deta h 
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+    
+}
